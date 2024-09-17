521a582eac4b5d2896e6028912de4da612be60ee
mjallow2021 <mjallow2021@outlook.com>
2024-09-17 12:10:34 +0100

    update reqs.txt file

--- getEnv.sh
+++ getEnv.sh
@@ -1,4 +1,5 @@
 SECRET_NAME="noftsite"
+ROOT_DIR="$(git rev-parse --show-toplevel)"
 
 SECRETS = $(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query SecretString --output text)
 touch $ENV_DIR
