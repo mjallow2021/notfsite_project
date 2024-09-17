01edeec39f1af793db90dc0bf421b1e10b16688e
mjallow2021 <mjallow2021@outlook.com>
2024-09-17 12:56:53 +0100

    send attemt remove spaces from  getEnv script

--- getEnv.sh
+++ getEnv.sh
@@ -1,7 +1,8 @@
 SECRET_NAME="noftsite_config_secrets"
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 
-SECRETS = $(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query SecretString --output text)
+SECRETS=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query SecretString --output text)
+ENV_DIR="$ROOT_DIR/.env"
 touch $ENV_DIR
 echo $SECRETS | jq -r 'to_entries | map("\(.key)=\(.value|tostring)") | .[]' > $ENV_DIR
 echo "Secrets written to $ENV_DIR"
